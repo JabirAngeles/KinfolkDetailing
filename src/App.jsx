d23da8169edf240629db4e6c34859eafecbb2180
DereckAn <dereckangeles15071998@gmail.com>
2023-05-27 21:49:46 -0600

    pc dereck

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Hero />
       </div>
       <Residencies />
-      <Value />
+      <Value/>
     </div>
   );
 }
--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,21 +1,23 @@
 import React from "react";
-import "./Value.css";
 import {
   Accordion,
   AccordionItem,
-  AccordionitemHeading,
+  AccordionItemHeading,
   AccordionItemButton,
   AccordionItemPanel,
   AccordionItemState,
 } from "react-accessible-accordion";
+import "./Value.css";
 import "react-accessible-accordion/dist/fancy-example.css";
 import { MdOutlineArrowDropDown } from "react-icons/md";
+import data from "../../utils/accordion";
+import {useState} from "react";
 
 const Value = () => {
   return (
     <section className="v-wrapper">
       <div className="paddings innerWidth flexCenter v-container">
-
+        
         {/*Left side*/}
         <div className="v-left">
           <div className="image-container">
@@ -24,7 +26,54 @@ const Value = () => {
         </div>
 
         {/*Right side*/}
-        
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">Value we give toyyi</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best services for you.
+            <br />
+            We believe a good service placeto live can make your life btter.
+          </span>
+
+          <Accordion
+            className="accordion"
+            allowMultipleExpanded={false}
+            preExpanded={[0]}
+          >
+            {data.map((item, i) => {
+              const [className, setClassName]= useState(null);
+
+              return (
+                <AccordionItem className={`accordionItem ${className}`} key={i} uuid={i}>
+                  
+                  <AccordionItemHeading>
+                    <AccordionItemButton className="flexCenter accordionButton">
+
+                      <AccordionItemState>
+                        {({ expanded }) =>
+                          expanded
+                            ? setClassName("expanded")
+                            : setClassName("collapse")
+                        }
+                      </AccordionItemState>
+                      
+                      <div className="flexCenter icon"> {item.icon}</div>
+                      <span className="primaryText">{item.heading}</span>
+                      <div className="flexCenter icon">
+                        <MdOutlineArrowDropDown size={20} />
+                      </div>
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+
+                  <AccordionItemPanel>
+                    <p className="secondaryText">{item.detail}</p>
+                  </AccordionItemPanel>
+
+                </AccordionItem>
+              );
+            })}
+          </Accordion>
+        </div>
       </div>
     </section>
   );
