d54a43442f5b02f352e2ad894c10bde44fa19869
JabirAngeles <Jabirangeles00@gmail.com>
2023-07-11 17:55:35 -0600

    New website business

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,6 @@ import Important from "./components/Important/Important";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Availability from "./components/Availability/Availability";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAL9iG1AQuzRJgonLuob5ploWInSEYFz7U",
-  authDomain: "kinfolk-12570.firebaseapp.com",
-  databaseURL: "https://kinfolk-12570-default-rtdb.firebaseio.com",
-  projectId: "kinfolk-12570",
-  storageBucket: "kinfolk-12570.appspot.com",
-  messagingSenderId: "222932835268",
-  appId: "1:222932835268:web:49cf4c517f8b0ae72a2291",
-  measurementId: "G-N5YVLM79WE",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 function App() {
   return (
     <div className="App">
