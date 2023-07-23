ef4e321412970d47a6d2a1eb1b8008da051a0a58
Jabir <Jabirangeles00@gmail.com>
2023-07-23 17:25:17 -0600

    olaaasa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "http://JabirAngeles.github.io/KinfolkDetailing",
+  base: "https://jabirangeles.github.io/kinfolkdetailing",
 });
