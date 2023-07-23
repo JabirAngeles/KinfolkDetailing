f960606c420f2dadba0b50d1f8cfa2a1a9388abf
Jabir <Jabirangeles00@gmail.com>
2023-07-23 16:29:22 -0600

    Deleting firebase docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://JabirAngeles.github.io/KinfolkDetailing",
+  base: "http://JabirAngeles.github.io/KinfolkDetailing",
 });
